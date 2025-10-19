378ec0e45fe69a55593156c1232b46b199eafe6e
FerrisOfficial <maciek.m.stempniak@gmail.com>
2025-10-19 22:11:58 +0200

    Added example for preprocessing to do later

--- game_simulation/board.cpp
+++ game_simulation/board.cpp
@@ -10,6 +10,16 @@ struct BoardState {
     Hex::PackedHex hexes[HEX_COUNT] = {0};
     Node::PackedNode nodes[NODE_COUNT] = {0};  
     Edge::PackedEdge edges[EDGE_COUNT] = {0};
+
+    BoardState() {
+        nodes[0] = Node::makeNode(5, 10, 15);
+        nodes[1] = Node::makeNode(0, 1, 2);
+        //itd
+
+        edges[0] = Edge::makeEdge(0, 1);
+        edges[1] = Edge::makeEdge(1, 2);
+        // itd
+    }
 };
 
 } // namespace Board
