be77ad03d94e591a9cd8b7bae3f2693c1c717af8
Yaryna Rachkevych <rarynka28@gmail.com>
2025-12-29 12:10:06 +0100

    Add log to se all legal actions at the moment

--- players/randomPlayer.cpp
+++ players/randomPlayer.cpp
@@ -34,6 +34,14 @@ Action::PackedAction RandomPlayer::getMoveRobber() {
 
 Action::PackedAction RandomPlayer::getTurnAction() {
     auto actions = boardState->getLegalActions(boardState->currentPlayer);
+    // print all legal actions
+    if (actions.empty()) {
+        logger.error("No legal actions available for current player!");
+        return Action::getEmptyAction();
+    }
+    for (const auto& action : actions) {
+        logger.log("Legal action: ", actionTypeName(Action::unpackType(action)));
+    }
     auto noAction = Action::getEmptyAction();
     int idx = RandomDevice::uniform_u32_range(0, static_cast<uint32_t>(actions.size()) - 1);
     return actions[idx];
